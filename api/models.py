cb2fd2b08db2a57eb8aa4ed4b629c6c5f92364db
hongdnn <namhongdoan99@gmail.com>
2024-10-04 19:30:35 -0700

    change table user

--- api/models.py
+++ api/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     id = models.IntegerField(primary_key=True)
-    email = models.TextField(null=True)
-    first_name = models.TextField()
-    last_name = models.TextField()
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=10, blank=True, null=True)
 
 
+    class Meta:
+        db_table = 'user'
